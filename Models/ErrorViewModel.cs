44b546e527f8924159ec54781a7ae4ce1bde4570
Benjamin Ek <benjaminek@live.se>
2021-11-02 16:12:22 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NordicStationCodeTestPart3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
